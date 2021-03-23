d7890158f2fe823374b50b1c39a8bffed1f84d0d
Shuliak Leonid <zeleny777@gmail.com>
2021-03-23 13:53:13 +0300

    add tailwind postcss at project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import * as ROUTES from './constants/routes'
 
-const App = () =>  <div> Hello </div>;
+const Login = lazy(() => import('./pages/Login')); // ленивая загрузка
 
+
+const App = () => (
+  <Router>
+    <Suspense fallback={<p>Loading...</p>} >
+    <Switch>
+      <Route path={ROUTES.LOGIN} component={Login} />
+    </Switch>
+    </Suspense>
+  </Router>
+);
 
 
 export default App;
---
+++ src/pages/Login.js
@@ -0,0 +1,26 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../context/firebase';
+
+const Login = () => {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [error, setError] = useState('');
+  const isInvalid = password === '' || emailAddress === '';
+  const handleLogin = () => {
+  };
+
+  useEffect(() => {
+    document.title = 'Login - Instagram';
+  }, []);
+  return (
+    < div className='container flex mx-auto max-w-screen-md items-center h-screen '>
+      Login;
+      Page
+    < /div>
+  );
+};
+export default Login;
