6e3c68318b6a8e63a9214f3c99a2cc6fe58bbdfa
Shuliak Leonid <zeleny777@gmail.com>
2021-03-23 12:00:40 +0300

    add firebase at project

---
+++ src/lib/firebase.js
@@ -0,0 +1,20 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { seedDatabase } from '../seed';
+
+const config = {
+  apiKey: 'AIzaSyAjO_QpX5TMBk6JppZA2vFWHZf19xl6DDg',
+  authDomain: 'instagram-a08cc.firebaseapp.com',
+  projectId: 'instagram-a08cc',
+  storageBucket: 'instagram-a08cc.appspot.com',
+  messagingSenderId: '47751896843',
+  appId: '1:47751896843:web:acfed29a7a59f4c52ecc0e'
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+
+console.log(firebase,'firebase');
+export { firebase, FieldValue };
