ff3b1540ddbd8c53ae3d9c631ecd3faa23bddc9d
Shuliak Leonid <zeleny777@gmail.com>
2021-03-23 17:40:44 +0300

    add component login with validation

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FirebaseContext } from '../context/firebase';
+import * as ROUTES from '../constants/routes';
 
 const Login = () => {
   const history = useHistory();
@@ -10,7 +11,16 @@ const Login = () => {
 
   const [error, setError] = useState('');
   const isInvalid = password === '' || emailAddress === '';
-  const handleLogin = () => {
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
+      history.push(ROUTES.DASHBOARD);
+    } catch (e) {
+      setEmailAddress('');
+      setPassword('');
+      setError(e.message);
+    }
   };
 
   useEffect(() => {
@@ -20,33 +30,44 @@ const Login = () => {
     < div className='container flex mx-auto max-w-screen-md items-center h-screen '>
       <div className='flex w-3/5'><img src='./images/iphone-with-profile.jpg' alt='iPhone with Instagram app' /></div>
       <div className='flex flex-col w-2/5'>
-        <h1 className='flex justify-center w-full'>
-          <img src='./images/logo.png' alt='Instagram' className='mt-2 w-6/12 mb-4' />
-        </h1>
-        {error && <p className='mb-4 text-xs text-red-primary '>{error}</p>}
+        <div className='flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded'>
+          <h1 className='flex justify-center w-full'>
+            <img src='./images/logo.png' alt='Instagram' className='mt-2 w-6/12 mb-4' />
+          </h1>
+          {error && <p className='mb-4 text-xs text-red-primary '>{error}</p>}
 
-        <form onSubmit={handleLogin} method='POST'>
-          <input
-            type='text'
-            onChange={({ target }) => setEmailAddress(target.value)}
-            placeholder='Email address'
-            aria-label='Enter your email address'
-            className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
-          />
-          <input
-            type='password'
-            onChange={({ target }) => setPassword(target.value)}
-            placeholder='Password'
-            aria-label='Enter you password'
-            className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
-          />
-          <button disabled={isInvalid}
-          type='submit'
-          className='bg-blue-medium text-white w-full rounded h-8 font-bold'
-          >
-Log In
-          </button>
-        </form>
+          <form onSubmit={handleLogin} method='POST'>
+            <input
+              type='text'
+              onChange={({ target }) => setEmailAddress(target.value)}
+              placeholder='Email address'
+              aria-label='Enter your email address'
+              className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
+            />
+            <input
+              type='password'
+              onChange={({ target }) => setPassword(target.value)}
+              placeholder='Password'
+              aria-label='Enter you password'
+              className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
+            />
+            <button disabled={isInvalid}
+                    type='submit'
+                    className={` bg-blue-medium text-white w-full rounded h-8 font-bold ${isInvalid && 'opacity-50'}`}
+            >
+              Log In
+            </button>
+          </form>
+        </div>
+        <div
+          className='flex justify-center items-center flex-col rounded w-full bg-white p-4 border border-gray-primary '>
+          <p className='text-sm'>
+            Don't have an account?{' '}
+            <Link to='/signup' className='font-bold text-blue-medium'>
+              Sign up
+            </Link>
+          </p>
+        </div>
       </div>
     < /div>
   );
--- src/seed.js
+++ src/seed.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-plusplus */
-// NOTE: replace 'NvPY9M9MzFTARQ6M816YAzDJxZ72' with your Firebase auth user id (can be taken from Firebase)
 export function seedDatabase(firebase) {
   const users = [
     {
