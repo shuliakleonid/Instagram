184a6ed64e6144729208d18cf779e4369dc7813b
Shuliak Leonid <zeleny777@gmail.com>
2021-04-11 22:23:18 +0300

    add timeline with comments

--- src/hooks/use-photos.js
+++ src/hooks/use-photos.js
@@ -2,24 +2,25 @@ import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../context/user';
 import { getPhotos, getUserByUserId } from '../services/firebase';
 
-export const usePhotos = () => {
+export const usePhotos = (user) => {
   const [photos, setPhotos] = useState(null);
-  const {
-    user: { uid: userId = '' }
-  } = useContext(UserContext);
+  // const {
+  //   user: { uid: userId = '' }
+  // } = useContext(UserContext);
 
   useEffect(() => {
     const getTimelinePhotos = async () => {
-      const [{following}] = await getUserByUserId(userId)
-      let followedUsersPhotos=[]
-      if(following.length > 0){
-        followedUsersPhotos = await getPhotos(userId,following)
-      }
+      // const [{following}] = await getUserByUserId(userId)
+      // let followedUsersPhotos=[]
+      if(user?.following?.length > 0){
+      const  followedUsersPhotos = await getPhotos(user.userId,user.following)
+
       followedUsersPhotos.sort((a, b) => b.dateCreated - a.dateCreated);
       setPhotos(followedUsersPhotos)
+      }
     };
     getTimelinePhotos()
-  },[userId]);
+  },[user?.userId]);
 
   return {photos}
 };
